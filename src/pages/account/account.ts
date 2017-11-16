1d1c38c07d9217929eff87b4206ad767cce0f2b9
Aslıhan Özfidan <aslihanozfidan@gmail.com>
2017-11-16 12:17:01 +0300

    refactoring account

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -5,13 +5,13 @@ import { ChangePasswordPage } from '../changepassword/changepassword';
 import { InformationPage } from '../information/information';
 import { WalletSettingsPage } from '../walletsettings/walletsettings';
 @Component({
-  selector: 'page-account',
-  templateUrl: 'account.html'
+    selector: 'page-account',
+    templateUrl: 'account.html'
 })
 export class AccountPage {
 
-  changePasswordRoot = ChangePasswordPage;
-  informationRoot = InformationPage;
-  walletSettingsRoot = WalletSettingsPage;
-  constructor(public navCtrl: NavController) {}
+    changePasswordRoot = ChangePasswordPage;
+    informationRoot = InformationPage;
+    walletSettingsRoot = WalletSettingsPage;
+    constructor(public navCtrl: NavController) {}
 }
